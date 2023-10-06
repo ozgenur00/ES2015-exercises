4c74f1a39a71e1f1b3abc48e378cb89bd15b42f8
Ozgenur Catal <117957198+ozgenur00@users.noreply.github.com>
2023-10-06 15:26:36 -0400

    arrow functions exercise added

--- es2015.js
+++ es2015.js
@@ -14,3 +14,31 @@ const PI = 3.14;
 
 //4. What is hosting?
 // Variables are "hoisted" to the top of their declaring scope, meaning you can access their names with an undefined value before they are used. Function declarations are also hoisted and can be called before they are defined in the code.
+
+//arrow functions
+
+//1. ES5 Map Callback
+
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+//   }
+
+//ES2015 version
+const DOUBLE = arr => arr.map(val => val * 2);
+
+//2. Refactor the following function to use arrow functions
+
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
+
+const SQUAREANDFINDEVENTS = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0);
+

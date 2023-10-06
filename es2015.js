9f14a66f64918c6cfcd6f58b4a16dc7487a16152
Ozgenur Catal <117957198+ozgenur00@users.noreply.github.com>
2023-10-06 18:11:15 -0400

    Rest and spread exercises added

--- es2015.js
+++ es2015.js
@@ -1,21 +1,25 @@
-// let and const
+// LET AND CONST
 
 const PI = 3.14;
 // it will not work if you try to change pi. But if we use var we can change it to anything.
 
+//----------------------------------------------------------------
 //1. What is the difference between var and let?
 //var - Can reassign and redeclare. But you can't redeclare using the let. Let creates block-scope.
 
+//----------------------------------------------------------------
 //2. What is the difference between var and const?
 //Var - It's function scoped, but constant cannot be changed once declared (immutable). Const has a block-scope too.
 
+//----------------------------------------------------------------
 //3. What is the difference between let and const?
 // You cannot redeclare or reassign using the const but you can redeclare with let.
 
+//----------------------------------------------------------------
 //4. What is hosting?
 // Variables are "hoisted" to the top of their declaring scope, meaning you can access their names with an undefined value before they are used. Function declarations are also hoisted and can be called before they are defined in the code.
 
-//arrow functions
+//ARROW FUNCTIONS
 
 //1. ES5 Map Callback
 
@@ -28,6 +32,7 @@ const PI = 3.14;
 //ES2015 version
 const DOUBLE = arr => arr.map(val => val * 2);
 
+//----------------------------------------------------------------
 //2. Refactor the following function to use arrow functions
 
 // function squareAndFindEvens(numbers){
@@ -42,3 +47,74 @@ const DOUBLE = arr => arr.map(val => val * 2);
 
 const SQUAREANDFINDEVENTS = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0);
 
+//REST AND SPREAD
+
+//1. ES5 function that takes a variable number of arguments(refactor it into ES2015)
+
+// function filterOutOdds() {
+//     var nums = Array.prototype.slice.call(arguments);
+//     return nums.filter(function(num) {
+//       return num % 2 === 0
+//     });
+//   }
+
+const FILTEROUTODDS = (...args) => args.filter(v => v % 2 === 0);
+
+//2. FindMin
+
+const FINDMIN = (...args) => Math.min(...args);
+
+//3. mergeObjects
+
+let mergeObjects = (obj1, obj2) => ({...obj1, ...obj2});
+
+//4. doubleAndReturnArgs
+
+ let doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map(val => val * 2)];
+
+ //5. slice and dice
+
+ /** remove a random element in the items array
+and return a new array without that item. */
+ let removeRandom = items => {
+    let index = Math.floor(Math.random() * items.length);
+    return [...items.slice(0, index), ...items.slice(index + 1)];
+ }
+
+//----------------------------------------------------------------
+
+/** remove a random element in the items array
+and return a new array without that item. */
+let extend = (arr1, arr2) => {
+    return [...arr1, ...arr2];
+}
+
+//----------------------------------------------------------------
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+let addKeyvak = (obj, key, val) => {
+let newObject = { ...obj }
+    newObject[key] = val;
+    return newObject;
+}
+//----------------------------------------------------------------
+/** Return a new object with a key removed. */
+let removeKey = (obj, key) => {
+let removeObjKey = {...obj}
+delete obj[key];
+return removeObjKey;
+}
+//----------------------------------------------------------------
+/** Combine two objects and return a new object. */
+
+let combine = (obj1, obj2) => {
+    return {...obj1, ...obj2}
+}
+//----------------------------------------------------------------
+/** Return a new object with a modified key and value. */
+
+let update = (obj, key, val) => {
+    let updatedObj = {...obj}
+    updatedObj[key] = val;
+    return updatedObj;
+}

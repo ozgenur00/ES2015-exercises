113e36a1637ec73e10ed5063560436ff340d7403
Ozgenur Catal <117957198+ozgenur00@users.noreply.github.com>
2023-10-09 13:25:54 -0400

    Maps and sets exercises added

--- es2015.js
+++ es2015.js
@@ -260,3 +260,55 @@ raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre']);
 
 let receResults = ([first, second, third, ...rest]) => ({first, second, third, rest});
 
+//MAPS AND SETS
+
+//1. Quick questions
+//#1 - What does the following code return?
+
+new Set([1,1,2,2,3,4]) // {1, 2, 3, 4}
+
+//#2 - What does the following code return?
+
+[...new Set("referee")].join("") // 'ref'
+
+//#3 What does the Map m look like after running the following code?
+
+let m = new Map();
+m.set([1,2,3], true);
+m.set([1,2,3], false); // {Array(3) => true, Array(3) => false}
+
+//----------------------------------------------------------------
+
+//2. hasDuplicate - Write a function called hasDuplicate which accepts an array and returns true or false if that array contains a duplicate
+
+hasDuplicate([1,3,2,1]) // true
+hasDuplicate([1,5,-1,4]) // false
+
+let hasDuplicate = arr => new Set(arr).size !== arr.length;
+
+//----------------------------------------------------------------
+
+//3. vowelCount - Write a function called vowelCount which accepts a string and returns a map where the keys are numbers and the values are the count of the vowels in the sting
+
+vowelCount('awesome') // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
+vowelCount('Colt') // Map { 'o' => 1 }
+
+function vowel(char) {
+    return "aeiou".includes(char);
+}
+
+function vowelCount(string) {
+    let vowelMap = new Map();
+    for (let char of string) {
+        let lowerCasedChar = char.toLowerCase();
+        if (vowel(lowerCasedChar)) {
+            if(vowelMap.has(lowerCasedChar)) {
+                vowelMap.set(lowerCasedChar, vowelMap.get(lowerCasedChar) + 1);
+            } else {
+                vowelMap.set(lowerCasedChar, 1) 
+            }
+        }
+    }
+    return vowelMap;
+}
+
